f3e3fca13d1c110ffe826c406cdeb737d05eac65
Subhadip Hazra <146234744+Subhadip-Hazra@users.noreply.github.com>
2024-05-07 21:48:42 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -327,4 +327,4 @@ def chatbot():
         return jsonify({'error': str(e)}), 500
     
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=5000)
+    app.run(debug=True,host='0.0.0.0')
